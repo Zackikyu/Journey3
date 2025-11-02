d81753cedcdf49faed3606cc2503833bb42c1e99
suleyman2004 <dhannyyewhannyyew@gmail.com>
2025-11-02 16:12:16 +0700

    vg

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Navbar from '@/component/Navbar'
+
 import React from 'react'
 
 function page() {
   return (
     <div className='bg-white text-black'>
-        <Navbar></Navbar>
     </div>
   )
 }
